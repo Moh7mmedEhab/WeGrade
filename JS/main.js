4ff831b3502ff9569ba50b7f058cb0a8a25c8913
Moh7mmedEhab <mohammed.ehab.23062@gmail.com>
2024-09-28 00:05:20 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,32 @@
 let timer = document.querySelector("#countdown");
 
+let head = document.querySelector("#date");
+
+let week_days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let day = new Date().getDay();
+
+let year = new Date().getFullYear();
+
 setInterval(() => {
 
-    let time = new Date("May, 25, 2026, 12:00:00").getTime();
+    let time = new Date(`May, 25, 2026, 00:00:00`).getTime();
+
+    let time_cpy = new Date(`May, 25, 2026, 00:00:00`).getDay();
 
     let now = new Date().getTime();
 
     let dis = time - now;
+
+    if (dis < 0) {
+
+        clearInterval();
+
+        timer.innerText = "Graduated !!";
+
+    }
+
+    head.innerText = `${week_days[time_cpy]} | 30-9-2026`
 
     let days = Math.floor(dis / (1000 * 60 * 60 * 24));
 
@@ -18,12 +38,4 @@ setInterval(() => {
 
     timer.innerText = `${days.toString().padStart(2, "0")}d : ${(hours % 24).toString().padStart(2, "0")}h : ${(minutes % 60).toString().padStart(2, "0")}m : ${(seconds % 60).toString().padStart(2, "0")}s`;
 
-    if (dis < 0) {
-
-        clearInterval();
-
-        timer.innerText("The Time Has Been Arrived.");
-
-    }
-
-}, 1);
+}, 10);
